73a98af9a017abee2dd55859325e0c2ddb0aa396
payamtajdivand <payamt1919@gmail.com>
2025-12-15 20:30:46 +0330

    Save data and generate HTML for delivery options

---
+++ data/deliveryOptions.js
@@ -0,0 +1,17 @@
+export const deliveryOptions = [
+  {
+    deliveryDays: 7,
+    id: "1",
+    priceCents: 0,
+  },
+  {
+    deliveryDays: 3,
+    id: "2",
+    priceCents: 499,
+  },
+  {
+    deliveryDays: 1,
+    id: "3",
+    priceCents: 999,
+  },
+];
